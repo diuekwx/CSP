de75f8e9c4bd9e8b29e1fe02aaefe6167e75ce5d
Jerison <jerisonw@gmail.com>
2025-07-29 10:57:04 -0700

    cors link

--- backend/csphub/src/main/java/yeri/csphub/Security/SecurityConfig.java
+++ backend/csphub/src/main/java/yeri/csphub/Security/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://127.0.0.1:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://127.0.0.1:5173", "victorious-vibrancy-production-9b1c.up.railway.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Set-Cookie", "Authorization"));
